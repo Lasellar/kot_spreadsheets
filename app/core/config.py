3a70ef3142186ef9209466c40b7ee0a4dd73ea24
Maksim Polushin <btnchsky.sad@mail.ru>
2025-04-28 18:32:39 +0300

    Optional for linter

--- app/core/config.py
+++ app/core/config.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from pydantic import BaseSettings
 
 
@@ -7,17 +9,17 @@ class Settings(BaseSettings):
     database_url: str = "sqlite+aiosqlite:///./cat_charity_fund.db"
     secret: str = 'SECRET'
     jwt_lifetime_seconds: int = 3600
-    type: str | None = None
-    project_id: str | None = None
-    private_key_id: str | None = None
-    private_key: str | None = None
-    client_email: str | None = None
-    client_id: str | None = None
-    auth_uri: str | None = None
-    token_uri: str | None = None
-    auth_provider_x509_cert_url: str | None = None
-    client_x509_cert_url: str | None = None
-    email: str | None = None
+    type: Optional[str] = None
+    project_id: Optional[str] = None
+    private_key_id: Optional[str] = None
+    private_key: Optional[str] = None
+    client_email: Optional[str] = None
+    client_id: Optional[str] = None
+    auth_uri: Optional[str] = None
+    token_uri: Optional[str] = None
+    auth_provider_x509_cert_url: Optional[str] = None
+    client_x509_cert_url: Optional[str] = None
+    email: Optional[str] = None
 
     class Config:
         env_file = '.env'
